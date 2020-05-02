40f1901ae8421e19596179dbf69185bc4e456de6
kisasexypantera94 <green.grinya@gmail.com>
2020-05-02 13:08:11 +0300

    use my own traverse

--- main.go
+++ main.go
@@ -8,6 +8,15 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func getCommit(hash string, repo *git.Repository) (*git.Commit, error) {
+	obj, err := repo.RevparseSingle(hash)
+	if err != nil {
+		return nil, err
+	}
+	commit, err := obj.AsCommit()
+	return commit, err
+}
+
 func getParents(commit *git.Commit) []*git.Commit {
 	parents := make([]*git.Commit, commit.ParentCount())
 	for i := 0; i < len(parents); i++ {
@@ -16,37 +25,53 @@ func getParents(commit *git.Commit) []*git.Commit {
 	return parents
 }
 
-func mapParentsToChildren(start string, repo *git.Repository) (map[string]string, error) {
-	walker, err := repo.Walk()
-	if err != nil {
-		return nil, err
+// iterateUntilVisited fills children map.
+// It does DFS until  all the places are visited
+// and then returns oldest place visited.
+func iterateUntilVisited(
+	commit *git.Commit,
+	places map[string]bool,
+	counter int,
+	children map[string]string,
+) (oldest *git.Commit, err error) {
+	cid := commit.Id().String()
+	if visited, ok := places[cid]; ok && !visited {
+		counter += 1
 	}
 
-	// Map parents to children
-	children := make(map[string]string)
-	err = walker.PushRange(fmt.Sprintf("%s..HEAD", start))
-	if err != nil {
-		return nil, err
+	if counter == len(places) {
+		return commit, nil
 	}
-	err = walker.Iterate(func(commit *git.Commit) bool {
-		parents := getParents(commit)
-		for _, p := range parents {
-			pid := p.Id().String()
-			children[pid] = commit.Id().String()
+
+	parents := getParents(commit)
+	for _, p := range parents {
+		pid := p.Id().String()
+		children[pid] = commit.Id().String()
+		pc, err := p.AsCommit()
+		if err != nil {
+			return nil, err
 		}
-		return true
-	})
+		oldest, err = iterateUntilVisited(pc, places, counter, children)
+		if err != nil {
+			return nil, err
+		}
+		if oldest != nil {
+			return oldest, nil
+		}
+	}
 
-	return children, err
+	return nil, nil
 }
 
-func getCommit(hash string, repo *git.Repository) (*git.Commit, error) {
-	obj, err := repo.RevparseSingle(hash)
+func mapParentsToChildren(start string, repo *git.Repository) (map[string]string, *git.Commit, error) {
+	children := make(map[string]string)
+	places := map[string]bool{start: false}
+	head, err := getCommit("HEAD", repo)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	commit, err := obj.AsCommit()
-	return commit, err
+	oldest, err := iterateUntilVisited(head, places, 0, children)
+	return children, oldest, err
 }
 
 func update(
@@ -88,7 +113,6 @@ func update(
 	}
 
 	newHead := oid
-	//fmt.Println(hash, oid.String())
 	if ch, ok := children[hash]; ok {
 		res, err := update(ch, oid.String(), hash, repo, children, newMsg)
 		if err != nil {
@@ -133,11 +157,11 @@ func main() {
 			newMsg := make(map[string]string)
 			newMsg[start] = msg
 
-			children, err := mapParentsToChildren(start, repo)
+			children, oldest, err := mapParentsToChildren(start, repo)
 			if err != nil {
 				return err
 			}
-			newHead, err := update(start, "", "", repo, children, newMsg)
+			newHead, err := update(oldest.Id().String(), "", "", repo, children, newMsg)
 			if err != nil {
 				return err
 			}
